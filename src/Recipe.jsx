0de9e85d232ea0eb2355cb6acb98270dbd078457
unknown <KaranKumarr>
2021-03-05 22:15:00 +0500

    Minor changes

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Recipe = (props) => {
-  const calories = Math.floor(props.calories);
+  let calories = Math.floor(props.calories);
   return (
     <>
       <div className="recipe">
@@ -9,7 +9,9 @@ const Recipe = (props) => {
           <h1 className="recipe-title">{props.title}</h1>
         </a>
 
-        <img src={props.imgLink} alt="img" className="image" />
+        <a href={props.recipeLink}>
+          <img src={props.imgLink} alt="img" className="image" />
+        </a>
         <p className="recipe-desc">{props.ingredients}</p>
         <p className="recipe-desc">Calories: {calories}</p>
       </div>
